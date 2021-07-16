0e8f0edb3dfe3c13391d0c0ed72e00e572a4fbb0
yhtiyar <yhtiyarsahatow@gmail.com>
2021-07-16 18:42:33 +0300

    removed unneeded visitor

--- python/infestor/test_transformers.py
+++ python/infestor/test_transformers.py
@@ -11,33 +11,6 @@ from .operations import add_reporter
 from .testcase import InfestorTestCase
 
 
-class CheckExceptHandlerVisitor(cst.CSTVisitor):
-
-    def __init__(self, reporter_imported_as):
-        self.reporter_imported_as = reporter_imported_as
-
-    def visit_ExceptHandler(self, node: cst.ExceptHandler) -> Optional[bool]:
-        if node.type is None:
-            raise Exception("Missing except type")
-        asname = None
-        try:
-            assert isinstance(node.name, cst.AsName)
-            assert isinstance(node.name.name, cst.Name)
-            asname = node.name.name.value
-        except:
-            raise Exception("Missing asname of exception")
-
-        has_error_report = False
-        for el in node.body.body:
-            if (
-                isinstance(el, cst.SimpleStatementLine)
-                and matches_error_report_statement(el, asname, self.reporter_imported_as)
-            ):
-                if has_error_report:
-                    raise Exception("error_report is called more than once ")
-                has_error_report = True
-        return True
-
 
 class TestTryExcept(unittest.TestCase):
     def setUp(self) -> None:
