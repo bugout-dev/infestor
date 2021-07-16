454a9f8d3542716178bc3a9982ffd5aa95658f32
yhtiyar <yhtiyarsahatow@gmail.com>
2021-07-16 15:56:22 +0300

    updadet fixtures

--- python/infestor/fixtures/a_package/cli.py
+++ python/infestor/fixtures/a_package/cli.py
@@ -3,12 +3,17 @@ import sys
 
 
 def generate_parser() -> argparse.ArgumentParser:
+    try:
+        l = 1/0
+    except:
+        print("It should fail")
     parser = argparse.ArgumentParser(description="greeter")
     parser.add_argument("name")
     return parser
 
 
 def main() -> None:
+
     parser = generate_parser()
     args = parser.parse_args()
     print(f"Hello, {args.name}. Information about the Python you are using:")
